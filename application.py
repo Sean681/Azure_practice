1b63d74fb2c4758d0ab5ccae686e659278a2ad3e
Sean681 <charger339ggl@gmail.com>
2021-08-08 05:16:41 +0000

    debug application.py

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #     "hello world"
 #     return "Hello World!!!!!"
 
-LINE_SECRET = "6d024ea68b260782527596c92c443698"]
+LINE_SECRET = "6d024ea68b260782527596c92c443698"
 LINE_TOKEN = "i+oLN4OdIOM4KdzooU2ydtVCEcRpS3pNoGeVxe0K1fkiBNwZmfMpnxbbIVW+2iatPzv0bSzmwInx/5u5ijXZcMHXdxS8h8uP+uC1ASzsUp2Wj3i1BIp+d1dAyRXisyPmFzqpgSehJj59/Do3bX2z8QdB04t89/1O/w1cDnyilFU="
 LINE_BOT = LineBotApi(LINE_TOKEN)
 HANDLER = WebhookHandler(LINE_SECRET)
