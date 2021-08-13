f21cc8eb6927fa9dd9193ddfc2da36c79219b510
Sean681 <charger339ggl@gmail.com>
2021-08-13 12:19:05 +0000

    fix application.py

--- application.py
+++ application.py
@@ -6,7 +6,9 @@ from linebot.models import (
     MessageEvent,
     TextMessage,
     TextSendMessage,
+    FlexSendMessage
 )
+import json
 
 app = Flask(__name__)
 
@@ -37,15 +39,26 @@ def callback():
 # message 可以針對收到的訊息種類
 @HANDLER.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
     url_dict = {
       "TIBAME":"https://www.tibame.com/coursegoodjob/traffic_cli", 
       "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/",
       "YOUTUBE":"https://www.youtube.com/"}
-# 將要發出去的文字變成TextSendMessage
-    try:
+    # 將要發出去的文字變成TextSendMessage
+    if event.message.text.upper() in url_dict.keys():
         url = url_dict[event.message.text.upper()]
         message = TextSendMessage(text=url)
-    except:
+        LINE_BOT.reply_message(event.reply_token, message)
+
+    elif event.message.text,upper() == "WISDOM":
+        with open("templates/flex_test.json", "r") as f_r:
+            bubble = json.load(f_r)
+        f_r.close()
+        LINE_BOT.reply_message(
+            event.reply_token,
+            [FlexSendMessage(alt_text="Report", contents=bubble)]
+        )
+    else:
         message = TextSendMessage(text=event.message.text)
-# 回覆訊息
-    LINE_BOT.reply_message(event.reply_token, message)
+        # 回覆訊息
+        LINE_BOT.reply_message(event.reply_token, message)
